8a6f38cdf0819ad9ae8b9ead29809fa7ff14cce8
Sahid Jamal <135206295+SahidJamal@users.noreply.github.com>
2024-04-06 17:22:35 +0530

    new

---
+++ qr_code_app/src/App.js
@@ -0,0 +1,48 @@
+import React, { useState, useRef } from "react";
+import ReactDOM from "react-dom";
+import QRCode from "react-qr-code";
+
+const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const qrCodeRef = useRef(null);
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleDownload = () => {
+    const canvas = qrCodeRef.current.getElementsByTagName("canvas")[0];
+    const url = canvas.toDataURL("image/png");
+    const downloadLink = document.createElement("a");
+    downloadLink.href = url;
+    downloadLink.download = "qrcode.png";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };
+
+  return (
+    <div style={{ padding: "30px", margin: "0px 0px 0px 0px" }}>
+      <div>
+        <input
+          style={{ width: "150px", height: "30px" }}
+          type="text"
+          value={inputValue}
+          onChange={handleChange}
+          placeholder="Enter URL for QR code"
+        />
+      </div>
+      <div style={{ marginTop: "15px" }}>
+        {inputValue && <QRCode value={inputValue} ref={qrCodeRef} />}
+      </div>
+      {/* {inputValue && (
+        <button style={{ marginTop: "15px" }} onClick={handleDownload}>
+          Download QR Code
+        </button>
+      )} */}
+      <p>All the best QR generators </p>
+    </div>
+  );
+};
+
+export default App;
